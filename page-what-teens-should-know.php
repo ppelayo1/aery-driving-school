1cb55304ca77cb74c9b30242f40a9a210db8ef05
Patrick Pelayo <PatrickP@PatrickP.tech>
2022-02-06 19:10:43 -0800

    teens know page finished, need to cleanup some code, then start transitioning toward a desktop view

---
+++ page-what-teens-should-know.php
@@ -0,0 +1,32 @@
+<?php get_header();?>
+
+<main>
+   
+    <h1 class='text-center margin-top-5rem'>What Teens Should Know</h1>
+    <div class='container margin-top-5rem'>
+        <h2 class='text-center'>How To Get Licensed</h2>
+        <div class='mt-5'>
+            <h3>Get The Permit</h3>
+            <p>When teenagers turn 15 years old, they are eligible to enroll and begin to take driving education. At 15  1/2 years old, they are eligible to take the written test. </p>
+        </div>
+        <div class='mt-4'>
+            <h3>Practice Driving</h3>
+            <p>Once they passed their test, the DMV will issue them a provisional permit. The permit is valid until a driving instructor signs it on their first driving lesson.  After the permit is validated, the teen can practice driving with an adult 25 years of age or older who has a valid California Driving's License. </p>
+        </div>
+        <div class='mt-4'>
+            <h3>Become Licensed</h3>
+            <p>After 6 months of the permit issue date and the minor has completed a minimum of 6 hours of behind-the-wheel driving training with a licensed instructor and has fulfilled 50 hours of driving practice with a licensed parent/guardian, the teen is eligible to take the driving test to get his/her Driver's License.</p>
+        </div>
+    </div>
+
+    <div class='container margin-top-5rem'>
+            <h2 class='text-center'>Our #1 Priortiy Is You</h2>
+            <p>Our desire is to not only help you pass the tests, but to also make sure that you will be a safe driver. We enjoy helping thousands of students in getting their license and to be prepared for the dangers that are on the road.</p>
+            <p>Our lessons are one-on-one in vehicles and you will be recieve instruction from a DMV approved instructor. As long as you are in our service area we will pick you up and drop you off.</p>
+            <div class="container enrollmentWrapper enrollmentTopPage text-center mt-4">
+            <a class="enrollmentLink" href=""> Enroll Now </a>
+        </div>
+    </div>
+</main>
+
+<?php get_footer();?>
