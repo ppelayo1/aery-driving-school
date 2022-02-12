12ab80062df634c0313e22aabab249fe98d40c54
Patrick Pelayo <PatrickP@PatrickP.tech>
2022-02-11 22:08:52 -0800

    fix typos

--- page-what-teens-should-know.php
+++ page-what-teens-should-know.php
@@ -22,8 +22,8 @@
 
     <div class='container margin-top-5rem max-width-40rem'>
             <h2 class='text-center'>Our #1 Priortiy Is You</h2>
-            <p>Our desire is to not only help you pass the tests, but to also make sure that you will be a safe driver. We enjoy helping thousands of students in getting their license and to be prepared for the dangers that are on the road.</p>
-            <p>Our lessons are one-on-one in vehicles and you will be recieve instruction from a DMV approved instructor. As long as you are in our service area we will pick you up and drop you off.</p>
+            <p>Our desire is to not only help you pass the tests, but to also make sure that you will be a safe driver. We are proud to have helped thousands of students in getting their licenses and in preparing them for the dangers on the road.</p>
+            <p>Your lessons will be one-on-one in our vehicles and you will recieve instruction from a DMV approved instructor. As long as you are in our service area we will pick you up and drop you off.</p>
             <div class="container text-center mt-4">
             <a class="btn btn-primary enrollmentLink enrollmentButtonColor" href=<?php echo '"' . getEnrollmentLink() . '"'; ?>> Enroll Now </a>
         </div>
